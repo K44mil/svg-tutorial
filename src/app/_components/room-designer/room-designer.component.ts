8af86fbf078778cef514f486e7d64042d0911c27
K44mil <kd155@o2.pl>
2019-08-22 11:33:42 +0200

    drag and drom tests

--- src/app/_components/room-designer/room-designer.component.ts
+++ src/app/_components/room-designer/room-designer.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Desk } from 'src/app/_models/Desk';
+import { Key } from 'protractor';
 
 @Component({
   selector: 'app-room-designer',
@@ -13,19 +14,49 @@ export class RoomDesignerComponent implements OnInit {
   selectedDeskPoint: SVGPoint = { x: 0, y: 0} as any;
   desks: Desk[] = [];
 
-  roomHeight = 500;
-  roomWidth = 500;
+  spaceWidth = 200;
+  spaceHeight = 200;
+
+  roomHeight = 1000;
+  roomWidth = 1000;
+  worker: string = "Jan Kowalski\nProgrammer\n";
 
   viewBox = "0 0 " + this.roomWidth.toString() + " " + this.roomHeight.toString();
 
   constructor() { }
 
   ngOnInit() {
-    let desk = new Desk(1, 100, 50, 0, 0, 1, 1);
+    let desk = new Desk(1, 50, 100, 0, 0, -1, 1, 0);
+    let _desk = new Desk(2, 50, 100, 150, 150, 1, 1, 0);
+    let __desk = new Desk(3, 50, 100, 250, 250, 1, 1, 0);
+    let ___desk = new Desk(4, 50, 100, 350, 350, -1, 1, 0);
+
     this.desks.push(desk);
+    this.desks.push(_desk);
+    this.desks.push(__desk);
+    this.desks.push(___desk);
   }
 
-  onSvgMouseClick(e: MouseEvent) {
+  onSvgMouseLeave(e: MouseEvent) {
+    
+    this.desks.forEach(desk => {
+      if (desk.positionX < 0) {
+        desk.positionX = 1;
+      }
+      if (desk.positionX + desk.width > this.roomWidth) {
+        desk.positionX = this.roomWidth - desk.width - 1;
+      }
+      if (desk.positionY < 0) {
+        desk.positionY = 1;
+      }
+      if (desk.positionY + desk.height > this.roomHeight) {
+        desk.positionY = this.roomHeight - desk.height - 1;
+      }
+    });
+
+  }
+
+  onSvgMouseUp(e: MouseEvent) {
 
       let svgElement = e.target as SVGElement;
       let svgSvgElement = e.target as SVGSVGElement;
@@ -44,10 +75,9 @@ export class RoomDesignerComponent implements OnInit {
       this.selectedDesk = null;
       this.selectedDeskElement = null;
 
-      console.log('-------------------------------------------------------------------');
+      console.log('--------------------SVG POINT--------------------');
       console.log('%cSelected desk: ' + this.selectedDesk, 'color: blue; font-weight: bold');
       console.log('%cCORDS: ' + '\nX: ' + point.x + '\nY: ' + point.y, 'color: blue; font-weight: 900;');
-
   }
 
   onSvgMouseMove(e: MouseEvent) {
@@ -71,36 +101,96 @@ export class RoomDesignerComponent implements OnInit {
       
       selectedDeskBorders.x = this.selectedDeskElement.getBoundingClientRect().left;
       selectedDeskBorders.y = this.selectedDeskElement.getBoundingClientRect().top;
+      selectedDeskBorders.w = this.selectedDeskElement.getBoundingClientRect().right;
+      selectedDeskBorders.z = this.selectedDeskElement.getBoundingClientRect().bottom;
 
       selectedDeskBorders = selectedDeskBorders.matrixTransform(svgSvgElement.getScreenCTM().inverse());
 
       // pozmieniać warunki, osobno dla poruszania sie w prawo i lewo
 
-      if (selectedDeskBorders.x > 0 && selectedDeskBorders.x + this.selectedDesk.length < this.roomHeight + 1) {
+      // if (selectedDeskBorders.x >= 0 && selectedDeskBorders.x + this.selectedDesk.width <= this.roomWidth) {
         this.selectedDesk.positionX = point.x - this.selectedDeskPoint.x;
+      // }
+      // if (selectedDeskBorders.y >= 0 && selectedDeskBorders.y + this.selectedDesk.height <= this.roomHeight) {
+        this.selectedDesk.positionY = point.y - this.selectedDeskPoint.y;
+      // }
+
+
+      // ustawienie krawedzi
+      if (selectedDeskBorders.x < 0) {
+        this.selectedDesk.positionX = 1;
+      }
+      if (selectedDeskBorders.x + this.selectedDesk.width > this.roomWidth) {
+        this.selectedDesk.positionX = this.roomWidth - this.selectedDesk.width - 1;
+      }
+      if (selectedDeskBorders.y < 0) {
+        this.selectedDesk.positionY = 1;
+      }
+      if (selectedDeskBorders.y + this.selectedDesk.height > this.roomHeight) {
+        this.selectedDesk.positionY = this.roomHeight - this.selectedDesk.height - 1;
       }
       
+      console.log('-----------------------RECT BORDERS-----------------------------------');
+      console.log('%cSelected desk: ' + this.selectedDesk, 'color: red; font-weight: bold');
+      console.log('%cCORDS: ' + '\nLeft: ' + selectedDeskBorders.x + '\nTop: ' + selectedDeskBorders.y + '\nRight: ' + (selectedDeskBorders.x + this.selectedDesk.width) + '\nBottom: ' + (selectedDeskBorders.y + this.selectedDesk.height), 'color: red; font-weight: 900;');
 
     }
   }
 
-  onRectMousClick(e: MouseEvent, clickedDesk: Desk) {
+  onRectMouseDown(e: MouseEvent, clickedDesk: Desk) {
     this.selectedDesk = this.desks.filter(x => x.id === clickedDesk.id)[0];
 
     const svgElement = e.target as SVGElement;
+
+    let svgSvgElement = e.target as SVGSVGElement;
+
+    if (!(svgElement instanceof SVGSVGElement)) {
+      svgSvgElement = svgElement.ownerSVGElement;
+    } 
+
     const rect = svgElement.getBoundingClientRect();
+    // ---------------------
+    let selectedDeskBorders = svgSvgElement.createSVGPoint();
+      
+    selectedDeskBorders.x = rect.left;
+    selectedDeskBorders.y = rect.top;
 
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
+    selectedDeskBorders = selectedDeskBorders.matrixTransform(svgSvgElement.getScreenCTM().inverse());
+    // --------------------------
+    let point = svgSvgElement.createSVGPoint();
 
-    this.selectedDeskPoint.x = x;
-    this.selectedDeskPoint.y = y;
+    point.x = e.clientX;
+    point.y = e.clientY;
+
+    point = point.matrixTransform(svgSvgElement.getScreenCTM().inverse());
+    //--------------------------------
+
+    const xLeft =  selectedDeskBorders.x;
+    const yTop =  selectedDeskBorders.y;
+
+    this.selectedDeskPoint.x = point.x - xLeft;
+    this.selectedDeskPoint.y = point.y - yTop;
 
     this.selectedDeskElement = svgElement;
 
-    console.log('-------------------------------------------------------------------');
+    console.log('--------------------------RECT POINT---------------------------');
     console.log('%cSelected desk: ' + this.selectedDesk, 'color: green; font-weight: bold');
-    console.log('%cCORDS: ' + '\nX: ' + x + '\nY: ' + y, 'color: green; font-weight: 900;');
+    console.log('%cCORDS: ' + '\nX: ' + point.x + '\nY: ' + point.y, 'color: green; font-weight: 900;');
+  }
+
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) { 
+
+    if (event.key === 'r') {
+      this.desks.forEach(desk => {
+        desk.height += 10;
+      });
+    } else if (event.key === 't') {
+      this.desks.forEach(desk => {
+        desk.width += 10;
+      });
+    }
+
   }
 
 }
--- src/app/_models/Desk.ts
+++ src/app/_models/Desk.ts
@@ -1,29 +1,32 @@
 export class Desk {
 
     id: number;
-    length: number;
+    height: number;
     width: number;
     positionX: number;
     positionY: number;
+    collide: number;
     employeeId: number;
     roomId: number;
 
     constructor(
         id: number,
-        length: number,
+        height: number,
         width: number,
         positionX: number,
         positionY: number,
         employeeId: number,
-        roomId: number
+        roomId: number,
+        collide: number
     ) {
         this.id = id;
-        this.length = length;
+        this.height = height;
         this.width = width;
         this.positionX = positionX;
         this.positionY = positionY;
         this.employeeId = employeeId;
         this.roomId = roomId;
+        this.collide = collide;
     }
 
 }
